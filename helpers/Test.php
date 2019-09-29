a1a9db737d91e0abcab7697e00f881fe64b31c71
Jack Dark <jacknouatin1@gmail.com>
2019-09-29 20:52:59 +0200

    Test

--- helpers/Test.php
+++ helpers/Test.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 require_once __DIR__ . '/Db.php';
 $test =  new Db();
 $t = $test->insert('test', ['name' => 'Amaelle', 'email' => null]);
@@ -8,4 +8,10 @@ if($t){
     echo 'success';
 }else{
     echo 'Echec';
-}
+}*/
+
+$uri = "http://www.babor_version_beta.com/?view=register";
+$detach = explode('/', $uri);
+$path = str_replace(end($detach), '?view', $uri);
+
+echo $path;
